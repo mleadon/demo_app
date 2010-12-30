6b736d80ce932327248182d19494045fb480ff9a
Michael Leadon <mleadon@cirm.ca.gov>
2010-12-29 16:27:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ce0a30125674330e50b75e5b8fd7179c8e914e26cbbd1fed544d6385d53a0c33df3cd4c726f608dff78b78c9322b5328a37d3f6d9e6d117f2bbb7306dddb045a'
